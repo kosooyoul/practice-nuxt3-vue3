7546b86b3f6d4cb3a932fa830eedb818f66058b5
kosooyoul <kosooyoul@gmail.com>
2022-08-31 17:19:37 +0900

    🔧 Add. axios module, and update configs

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,13 @@
-
 import { defineNuxtConfig } from 'nuxt'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-modules: ["@pinia/nuxt", "@vueuse/nuxt"]
+
+  publicRuntimeConfig: {
+    // axios: {
+    //   baseURL: 'https://api.nuxtjs.dev',
+    // },
+  },
+
+  modules: ['@pinia/nuxt', '@vueuse/nuxt', '@nuxtjs-alt/axios'],
 })
