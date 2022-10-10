7ff8e7824a3643587179fbc4ad434273e17ab581
kosooyoul <kosooyoul@gmail.com>
2022-10-10 20:50:16 +0900

    🐱‍🚀 Upt. index, seperated each views to section

--- pages/index.vue
+++ pages/index.vue
@@ -81,31 +81,38 @@ export default {
 <template>
   <div>
     <section>
-      <h1>BubbleButtonView</h1>
+      <h2>1. BubbleButtonView</h2>
       <BubbleButtonView @click="log('BubbleButtonView#1@click')">안녕#1</BubbleButtonView>
       <BubbleButtonView @click="log('BubbleButtonView#2@click')">안녕#2</BubbleButtonView>
       <BubbleButtonView @click="log('BubbleButtonView#3@click')">안녕#3</BubbleButtonView>
       <BubbleButtonView @click="log('BubbleButtonView#4@click')">안녕#4</BubbleButtonView>
       <BubbleButtonView @click="log('BubbleButtonView#5@click')">안녕#5</BubbleButtonView>
     </section>
-    <ButtonView title="TODO 를 남겨요!" @click="showTodoInputDialog" />
-    <DialogView ref="todoInputDialogView">
-      <TodoInputView @item-confirmed="confirmTodoItem" />
-    </DialogView>
-    <TodoBoardView ref="todoBoardView" :items="todoItems" style="position: relative; z-index: 1;" @item-updated="updateTodoItem" />
-    <div style="height: 200px;" />
-    <WaveSvgView
-      bg-color="transparent"
-      drop-color="rgb(120, 255, 230)"
-      :rain-size="20"
-      :rain-freq="0.02"
-      :fps="40"
-      :enable-splash="false"
-      style="position: absolute; left: 0px; bottom: 0px; width: 100%; height: 200px; z-index: 0;"
-    />
+    <section>
+      <h2>2. TODO BoardView</h2>
+      <ButtonView title="TODO 를 남겨요!" @click="showTodoInputDialog" />
+      <DialogView ref="todoInputDialogView">
+        <TodoInputView @item-confirmed="confirmTodoItem" />
+      </DialogView>
+      <TodoBoardView ref="todoBoardView" :items="todoItems" style="position: relative; z-index: 1;" @item-updated="updateTodoItem" />
+    </section>
+    <section>
+      <h2>3. WaveSvgView</h2>
+      <WaveSvgView
+        bg-color="transparent"
+        drop-color="rgb(120, 255, 230)"
+        :rain-size="20"
+        :rain-freq="0.02"
+        :fps="40"
+        :enable-splash="false"
+        style="left: 0px; bottom: 0px; width: 100%; height: 200px; z-index: 0;"
+      />
+    </section>
   </div>
 </template>
 
 <style scoped>
-
+section {
+  margin-bottom: 40px;
+}
 </style>
