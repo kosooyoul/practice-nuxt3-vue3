c9ccef62363a912325586aef1fee0b4881ea52e9
kosooyoul <kosooyoul@gmail.com>
2022-10-13 21:46:56 +0900

    💄 Upt. page layout and style

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,7 +8,7 @@
 .layout {
   margin: 0px;
   padding: 20px;
-  background-image: url('http://blog.ahyane.net/images/background.png');
+  background-image: url('/public/background/background.png');
   background-size: 64px;
   background-color: #eee;
 }
--- pages/index.vue
+++ pages/index.vue
@@ -55,13 +55,16 @@ export default {
 
 <template>
   <div>
+    <header>
+      <h1>Nux3+Vue3 Components</h1>
+    </header>
     <section>
-    <section>
-      <h2>1. MyWebGLView</h2>
+      <h2>Working...</h2>
       <MyWebglView />
       <MyThreejsSampleView />
     </section>
-      <h2>2. OrbisView</h2>
+    <section>
+      <h2>4. OrbisView</h2>
       <OrbisView class="orbis-view" src="/public/textures/20180924.jpg" :width="300" :height="200" />
       <OrbisView class="orbis-view" :width="300" :height="200"/>
     </section>
@@ -72,7 +75,7 @@ export default {
       <BubbleButtonView class="bubble-button-view" @click="log('BubbleButtonView#3@click')">안녕#3</BubbleButtonView>
     </section>
     <section>
-      <h2>4. TODO BoardView</h2>
+      <h2>2. TODO BoardView</h2>
       <ButtonView title="TODO 를 남겨요!" @click="showTodoInputDialog" />
       <TodoBoardView class="todo-board-view" ref="todoBoardView" :items="todoItems" @item-updated="updateTodoItem" />
       <DialogView ref="todoInputDialogView">
@@ -80,23 +83,33 @@ export default {
       </DialogView>
     </section>
     <section>
-      <h2>5. WaveSvgView</h2>
-      <WaveSvgView
-        class="wave-svg-view"
-        bg-color="transparent"
-        drop-color="#50b4c8"
-        :rain-size="20"
-        :rain-freq="0.02"
-        :fps="40"
-        :enable-splash="false"
-      />
+      <h2>1. WaveSvgView</h2>
+      <WaveSvgView class="wave-svg-view" bg-color="transparent" drop-color="#50b4c8" :rain-size="20" :rain-freq="0.02" :fps="40" :enable-splash="false" />
     </section>
+    <footer>
+      <p class="sign">by Kosooyoul</p>
+    </footer>
   </div>
 </template>
 
 <style scoped>
 section {
+  margin-top: 40px;
   margin-bottom: 40px;
+}
+.orbis-view {
+  background-color: rgba(255, 255, 255, 0.6);
+  border: 1px solid #ddd;
+  border-radius: 4px;
+  box-shadow: 4px 4px 16px 0px rgba(0, 0, 0, 0.2);
+  margin: 10px;
+}
+.bubble-button-view {
+  margin: 10px;
+}
+.todo-board-view {
+  width: 1200px;
+  max-width: 100%;
 }
 .wave-svg-view {
   left: 0px;
@@ -105,18 +118,10 @@ section {
   max-width: 100%;
   height: 200px;
 }
-.todo-board-view {
-  width: 1200px;
-  max-width: 100%;
-}
-.bubble-button-view {
-  margin: 10px;
-}
-.orbis-view {
-  background-color: rgba(255, 255, 255, 0.6);
-  border: 1px solid #ddd;
-  border-radius: 4px;
-  box-shadow: 4px 4px 16px 0px rgba(0, 0, 0, 0.2);
-  margin: 10px;
+.sign {
+  font-size: 12px;
+  font-weight: bold;
+  color: white;
+  text-shadow: 0px -1px 2px #666666;
 }
 </style>
