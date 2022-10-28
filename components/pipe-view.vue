23be3428fb995de6c548e2148f208a96d648cebb
kosooyoul <kosooyoul@gmail.com>
2022-10-28 23:45:13 +0900

    🦝 Add. history page with pipe view

---
+++ components/pipe-view.vue
@@ -0,0 +1,120 @@
+<script lang="ts" setup>
+import { defineComponent, defineProps, withDefaults } from 'vue'
+
+interface PipeViewProps {
+  title?: string
+  description?: string
+  summaries?: {
+    title?: string
+    description?: string
+  }[]
+}
+
+withDefaults(defineProps<PipeViewProps>(), {
+  title: '',
+  description: '',
+  summaries: () => [{
+    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque sequi fugit aliquid. Accusantium temporibus explicabo quae earum doloremque odio dolor amet obcaecati. Ad at cumque quasi delectus tenetur, officiis illum!',
+  }, {
+    title: 'Lorem ipsum dolor sit amet consectetur adipisicing elit.',
+    description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque sequi fugit aliquid. Accusantium temporibus explicabo quae earum doloremque odio dolor amet obcaecati. Ad at cumque quasi delectus tenetur, officiis illum!',
+  }],
+})
+
+defineComponent({
+  name: 'PipeView',
+})
+</script>
+
+<template>
+  <div class="root">
+    <div class="pipe">
+      <div class="circle">
+        <h3 class="title" style="color: #000; font-weight: bold; font-size: 14px;">
+          {{ title }}
+        </h3>
+        <p class="description" style="color: #000; font-size: 12px;">
+          {{ description }}
+        </p>
+      </div>
+    </div>
+    <ul class="summaries">
+      <li v-for="(summary, i) in summaries" :key="i" class="summary">
+        <h4 class="title">
+          {{ summary.title }}
+        </h4>
+        <p class="description">
+          {{ summary.description }}
+        </p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+div.root {
+  display: flex;
+  position: relative;
+  align-content: center;
+  justify-content: flex-start;
+}
+div.pipe {
+  position: relative;
+}
+div.pipe::before {
+  content: ' ';
+  position: absolute;
+  width: 1px;
+  height: 100%;
+  left: 50%;
+  top: 0px;
+  border-left: 1px solid white;
+}
+div.circle {
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  align-content: center;
+  justify-content: center;
+  align-items: center;
+  width: 76px;
+  height: 76px;
+  margin: 32px auto;
+  border-radius: 40px;
+  border: 1px solid white;
+  background-color: #e0e0e0;
+  z-index: 1;
+}
+div.circle>h3.title {
+  margin: 0px;
+  padding: 0px;
+  font-weight: bold;
+  font-size: 14px;
+  color: black;
+}
+div.circle>p.description {
+  margin: 0px;
+  padding: 0px;
+  font-size: 12px;
+  color: black;
+}
+ul.summaries {
+  margin-left: 32px;
+  margin-top: 32px;
+  padding: 0px;
+  list-style: none;
+}
+li.summary {
+  margin: 24px 0px;
+}
+li.summary>h4.title {
+  margin: 8px 0px;
+  font-weight: bold;
+  font-size: 14px;
+}
+li.summary>p.description {
+  margin: 0px;
+  font-size: 14px;
+}
+</style>
---
+++ pages/history-demo.vue
@@ -0,0 +1,35 @@
+<script lang="ts">
+export default {
+  name: 'HistoryDemoPage',
+  setup(): void {
+
+  },
+}
+</script>
+
+<template>
+  <div>
+    <section style="display: flex; flex-direction: column; align-items: center; padding: 0px 24px; background-color: #7BD7C1;">
+      <h2 style="color: white; text-align: center; font-size: 24px; padding: 48px;">
+        HISTORY
+      </h2>
+      <ul style="width: 640px; max-width: 100%; margin: 0px; padding: 0px; list-style: none;">
+        <li>
+          <PipeView title="2022" description="Oct" />
+        </li>
+        <li>
+          <PipeView title="2022" description="Jun" />
+        </li>
+        <li>
+          <PipeView title="2022" description="Apr" />
+        </li>
+        <li>
+          <PipeView title="2021" description="May" />
+        </li>
+        <li>
+          <PipeView title="2020" description="Dec" />
+        </li>
+      </ul>
+    </section>
+  </div>
+</template>
